77a5e5c1e9e1f15a692243ef3cfeb96b679e8c37
Kirill Sobolev <sobolevkir@bk.ru>
2023-06-12 21:41:43 +0300

    The search field has been improved.

--- app/src/main/java/com/sobolevkir/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/sobolevkir/playlistmaker/SearchActivity.kt
@@ -1,11 +1,57 @@
 package com.sobolevkir.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<ImageButton>(R.id.button_back)
+        val inputSearch = findViewById<EditText>(R.id.inputSearch)
+        val clearButton = findViewById<ImageView>(R.id.clearButton)
+
+        backButton.setOnClickListener{ finish() }
+
+        clearButton.setOnClickListener{
+            inputSearch.setText("")
+            closeKeyboard()
+        }
+
+        val searchInputTextWatcher = object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+               clearButton.visibility = clearButtonVisibility(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        inputSearch.addTextChangedListener(searchInputTextWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun closeKeyboard() {
+        this.currentFocus?.run {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(this.windowToken, 0)
+        }
+    }
+
 }
