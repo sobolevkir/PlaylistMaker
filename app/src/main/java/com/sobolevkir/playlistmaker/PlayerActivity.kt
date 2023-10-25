4d363f9e49aede02d5db033e97c36e80e3e37b95
Kirill Sobolev <sobolevkir@bk.ru>
2023-10-25 15:47:46 +0300

    Использован ViewBinding для PlayerActivity. Скорректированы размеры и отступы в случае длинных названий. Цвета и размеры вынесены в ресурсы.

--- app/src/main/java/com/sobolevkir/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/sobolevkir/playlistmaker/PlayerActivity.kt
@@ -1,96 +1,70 @@
 package com.sobolevkir.playlistmaker
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.sobolevkir.playlistmaker.databinding.ActivityPlayerBinding
 import com.sobolevkir.playlistmaker.tracklist.Track
 
 class PlayerActivity : AppCompatActivity() {
 
-    private lateinit var backButton: ImageButton
-    private lateinit var albumCoverLarge: ImageView
-    private lateinit var trackName: TextView
-    private lateinit var artistName: TextView
-    private lateinit var trackDuration: TextView
-    private lateinit var collectionName: TextView
-    private lateinit var collectionNameTitle: TextView
-    private lateinit var releaseYear: TextView
-    private lateinit var releaseYearTitle: TextView
-    private lateinit var genre: TextView
-    private lateinit var genreTitle: TextView
-    private lateinit var country: TextView
-    private lateinit var countryTitle: TextView
+    companion object{
+        const val CURRENT_TRACK = "current_track"
+    }
+
+    private lateinit var binding: ActivityPlayerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        backButton = findViewById(R.id.btn_back)
-        backButton.setOnClickListener { finish() }
+        binding.btnBack.setOnClickListener { finish() }
 
         val track = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableExtra("current_track", Track::class.java)
-        } else intent.getParcelableExtra("current_track")
+            intent.getParcelableExtra(CURRENT_TRACK, Track::class.java)
+        } else intent.getParcelableExtra(CURRENT_TRACK)
 
-        albumCoverLarge = findViewById(R.id.iv_album_cover_large)
-        Glide.with(albumCoverLarge)
+
+        Glide.with(binding.ivAlbumCoverLarge)
             .load(track?.getLargeCoverArtwork())
             .centerInside()
             .placeholder(R.drawable.cover_placeholder)
-            .into(albumCoverLarge)
+            .into(binding.ivAlbumCoverLarge)
 
-        trackName = findViewById(R.id.tv_track_name)
-        trackName.text = track?.trackName
 
-        artistName = findViewById(R.id.tv_artist_name)
-        artistName.text = track?.artistName
+        binding.tvTrackName.text = track?.trackName
+        binding.tvArtistName.text = track?.artistName
+        binding.tvDurationValue.text = track?.getFormattedTime()
 
-        trackDuration = findViewById(R.id.tv_duration_value)
-        trackDuration.text = track?.getFormattedTime()
-
-        collectionName = findViewById(R.id.tv_collection_name_value)
-        collectionNameTitle = findViewById(R.id.tv_collection_name_title)
-        if ((track?.collectionName != null) && track.collectionName.isNotEmpty()) {
-            collectionName.text = track.collectionName
-        }
-        else {
-            collectionName.visibility = View.GONE
-            collectionNameTitle.visibility = View.GONE
+        if (!track?.collectionName.isNullOrEmpty()) {
+            binding.tvCollectionNameValue.text = track?.collectionName
+        } else {
+            binding.tvCollectionNameValue.visibility = View.GONE
+            binding.tvCollectionNameValue.visibility = View.GONE
         }
 
-        releaseYear = findViewById(R.id.tv_release_year_value)
-        releaseYearTitle = findViewById(R.id.tv_release_year_title)
-        if ((track?.releaseDate != null) && track.releaseDate.isNotEmpty()) {
-            releaseYear.text = track.getReleaseYear()
-        }
-        else {
-            releaseYear.visibility = View.GONE
-            releaseYearTitle.visibility = View.GONE
+        if (!track?.releaseDate.isNullOrEmpty()) {
+            binding.tvReleaseYearValue.text = track?.getReleaseYear()
+        } else {
+            binding.tvReleaseYearValue.visibility = View.GONE
+            binding.tvReleaseYearTitle.visibility = View.GONE
         }
 
-        genre = findViewById(R.id.tv_genre_value)
-        genreTitle = findViewById(R.id.tv_genre_title)
-        if ((track?.primaryGenreName != null) && track.primaryGenreName.isNotEmpty()) {
-            genre.text = track.primaryGenreName
-        }
-        else {
-            genre.visibility = View.GONE
-            genreTitle.visibility = View.GONE
+        if (!track?.primaryGenreName.isNullOrEmpty()) {
+            binding.tvGenreValue.text = track?.primaryGenreName
+        } else {
+            binding.tvGenreValue.visibility = View.GONE
+            binding.tvGenreTitle.visibility = View.GONE
         }
 
-        country = findViewById(R.id.tv_country_value)
-        countryTitle = findViewById(R.id.tv_country_title)
-        if ((track?.country != null) && track.country.isNotEmpty()) {
-            country.text = track.country
-        }
-        else {
-            country.visibility = View.GONE
-            country.visibility = View.GONE
+        if (!track?.country.isNullOrEmpty()) {
+            binding.tvCountryValue.text = track?.country
+        } else {
+            binding.tvCountryValue.visibility = View.GONE
+            binding.tvCountryTitle.visibility = View.GONE
         }
 
     }
